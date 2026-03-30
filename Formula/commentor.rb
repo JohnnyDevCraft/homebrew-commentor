c7ae4119ff3fc4e699d7fa55da3d7f4ed1a01458
Johnathon D Smith <jd49490@me.com>
2026-03-30 09:11:37 -0400

    Update Homebrew formula for Commentor 0.1.1
    Point the tap formula at the Commentor 0.1.1 macOS release assets and refresh the architecture-specific checksums so Homebrew installs and upgrades the latest published build.

--- Formula/commentor.rb
+++ Formula/commentor.rb
@@ -1,18 +1,18 @@
 class Commentor < Formula
   desc "Azure DevOps pull request comment triage utility"
   homepage "https://github.com/JohnnyDevCraft/commentor"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/JohnnyDevCraft/commentor/releases/download/v0.1.0/commentor-0.1.0-osx-arm64.tar.gz"
-      sha256 "59e3228c6c3369c437d719362f2756bd4165089b5cbe50b7b43a68ab663bb687"
+      url "https://github.com/JohnnyDevCraft/commentor/releases/download/v0.1.1/commentor-0.1.1-osx-arm64.tar.gz"
+      sha256 "ae61f645dd1eefaf0b7595e8a726e9a9bb2d68ad0cea1d3eca23127694bb8dbb"
     end
 
     on_intel do
-      url "https://github.com/JohnnyDevCraft/commentor/releases/download/v0.1.0/commentor-0.1.0-osx-x64.tar.gz"
-      sha256 "ca823b2cefeec2063080568356a9e1cad24ba970cdaeb2d49cbd84afdb2d6f06"
+      url "https://github.com/JohnnyDevCraft/commentor/releases/download/v0.1.1/commentor-0.1.1-osx-x64.tar.gz"
+      sha256 "aceb2d8575d87b9041d9e3fddba695a5a7855e18c22c8d921fcd5150ec53dfca"
     end
   end
 
